93779cf342062080fa55ac3d1f498b4ab7421de1
Laurent <haarlaurent67@hotmail.fr>
2023-05-03 22:28:22 +0200

    added stats HUD

--- src/body/mod.rs
+++ src/body/mod.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
+use std::sync::atomic::{AtomicU16, Ordering};
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::pixels::Color;
 use crate::util::Vector2;
+use sdl2::rect::Rect;
+use sdl2::video::WindowContext;
 
 #[derive(Clone)]
 pub struct Body
@@ -10,6 +12,7 @@ pub struct Body
     pub velocity: Vector2<f32>,
     pub mass: f32,
     pub radius: f32,
+    pub id: u16,
 }
 
 pub struct World
@@ -42,7 +45,7 @@ impl World
                     continue;
                 }
                 let r_vector = vec![body.position.x - second_body.position.x, body.position.y - second_body.position.y];
-                let r_mag = (r_vector[0] * r_vector[0] + r_vector[1] * r_vector[1]);
+                let r_mag = r_vector[0] * r_vector[0] + r_vector[1] * r_vector[1];
                 let r_mag = if r_mag < (body.radius + second_body.radius) * (body.radius + second_body.radius) {
                     body.radius + second_body.radius
                 } else {
@@ -65,22 +68,49 @@ impl World
         }
     }
 
+    fn render_text(str: &str, font: &mut sdl2::ttf::Font, texture_creator: &sdl2::render::TextureCreator<WindowContext>, target: Rect, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>)
+    {
+        let surface = font.render(str).blended(Color::RGB(255, 255, 255)).map_err(|e| e.to_string()).unwrap();
+        let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
+        canvas.copy(&texture, None, Some(target)).unwrap();
+    }
 
-    pub fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>)
+    pub fn draw(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, font: &mut sdl2::ttf::Font, selected_body: Option<u16>)
     {
         for body in &self.bodies
         {
             canvas.filled_circle(body.position.x as i16, body.position.y as i16, body.radius as i16, Color::RGB(255, (255.0 * (100.0 - body.mass) / 100.0) as u8, (255.0 * (100.0 - body.mass) / 100.0) as u8)).unwrap();
         }
+        if let Some(..) = selected_body {
+            let id = selected_body.unwrap() as usize;
+            let body = &self.bodies[id];
+            let pos = body.position;
+            let vel = body.velocity;
+            let rad = body.radius;
+            let mass = body.mass;
+            let texture_creator = canvas.texture_creator();
+            let target = Rect::new(0, 0, 100, 50);
+            Self::render_text(&format!("Body #{id}"), font, &texture_creator, target, canvas);
+            let target = Rect::new(0, 50, 300, 50);
+            Self::render_text(&format!("pos: ({:.2}; {:.2})", pos.x, pos.y), font, &texture_creator, target, canvas);
+            let target = Rect::new(0, 100, 300, 50);
+            Self::render_text(&format!("vel: ({:.2}; {:.2})", vel.x, vel.y), font, &texture_creator, target, canvas);
+            let target = Rect::new(0, 150, 100, 50);
+            Self::render_text(&format!("rad: {rad}"), font, &texture_creator, target, canvas);
+            let target = Rect::new(0, 200, 100, 50);
+            Self::render_text(&format!("mass: {mass}"), font, &texture_creator, target, canvas);
+        }
     }
 
     pub fn add_body(&mut self, x: i32, y: i32, mass: f32)
     {
+        static COUNTER: AtomicU16 = AtomicU16::new(0);
         self.bodies.push(Body {
             velocity: Vector2::new(0.0, 0.0),
             position: Vector2::new(x as f32, y as f32),
             mass,
             radius: 2.0,
+            id: COUNTER.fetch_add(1, Ordering::Relaxed),
         });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,62 +6,74 @@ use crate::body::World;
 mod body;
 mod util;
 
-fn running_events(wait_ms: &mut u64, running: &mut bool, event: &Event) -> bool
+fn running_events(appstate: &mut AppState, event: &Event) -> bool
 {
     match event {
         Event::Quit { .. } => {
             return true;
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Space), .. } => {
-            *running = false;
+            appstate.simulation_running = false;
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::F), .. } => {
-            *wait_ms /= 2;
+            appstate.update_ms /= 2;
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::G), .. } => {
-            *wait_ms *= 2;
+            appstate.update_ms *= 2;
+        }
+        Event::MouseButtonDown { mouse_btn: MouseButton::Left, x, y, .. } => {
+            let mut found = false;
+            for body in appstate.world.bodies.iter() {
+                if (*x as f32 - body.position.x).powf(2.) + (*y as f32 - body.position.y).powf(2.) < body.radius.powf(2.) {
+                    appstate.selected_body = Some(body.id);
+                    found = true;
+                }
+            }
+            if !found {
+                appstate.selected_body = None;
+            }
         }
         _ => {}
     }
     false
 }
 
-fn not_running_events(world: &mut World, running: &mut bool, mouse: MouseState, event: &Event) -> bool
+fn not_running_events(appstate: &mut AppState, event: &Event) -> bool
 {
     match event {
         Event::Quit { .. } => {
             return true;
         }
         Event::MouseButtonDown { mouse_btn: MouseButton::Left, x, y, .. } => {
-            world.add_body(*x, *y, 1.0);
+            appstate.world.add_body(*x, *y, 1.0);
         }
         Event::MouseButtonDown { mouse_btn: MouseButton::Right, x, y, .. } => {
-            world.add_body(*x, *y, 10.0);
+            appstate.world.add_body(*x, *y, 10.0);
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Space), .. } => {
-            world.save_state();
-            *running = true;
+            appstate.world.save_state();
+            appstate.simulation_running = true;
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::R), .. } => {
-            world.bodies = world.initial_bodies.clone();
+            appstate.world.bodies = appstate.world.initial_bodies.clone();
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::C), .. } => {
-            world.bodies.clear();
-            world.initial_bodies.clear();
+            appstate.world.bodies.clear();
+            appstate.world.initial_bodies.clear();
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::G), .. } => {
-            let x = mouse.x();
-            let y = mouse.y();
-            for body in world.bodies.iter_mut() {
+            let x = appstate.mouse_state.unwrap().x();
+            let y = appstate.mouse_state.unwrap().y();
+            for body in appstate.world.bodies.iter_mut() {
                 if body.position.x == x as f32 && body.position.y == y as f32 {
                     body.mass += 1.0;
                 }
             }
         }
         Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::F), .. } => {
-            let x = mouse.x();
-            let y = mouse.y();
-            for body in world.bodies.iter_mut() {
+            let x = appstate.mouse_state.unwrap().x();
+            let y = appstate.mouse_state.unwrap().y();
+            for body in appstate.world.bodies.iter_mut() {
                 if body.position.x == x as f32 && body.position.y == y as f32 {
                     body.radius += 1.0;
                 }
@@ -72,40 +84,54 @@ fn not_running_events(world: &mut World, running: &mut bool, mouse: MouseState, 
     false
 }
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+struct AppState {
+    pub world: World,
+    pub simulation_running: bool,
+    pub mouse_state: Option<MouseState>,
+    pub update_ms: u16,
+    pub selected_body: Option<u16>,
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     let window = video_subsystem.window("N-Body Simulation", 800, 600)
         .position_centered()
-        .opengl()
         .build()
         .unwrap();
-    let mut canvas = window.into_canvas().build().unwrap();
-    let mut world = World::new();
-    let mut wait_ms = 16;
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+    let mut font = ttf_context.load_font("res/fira.ttf", 128)?;
+    font.set_style(sdl2::ttf::FontStyle::NORMAL);
     canvas.set_draw_color(Color::BLACK);
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut running = false;
+    let mut event_pump = sdl_context.event_pump()?;
+    let mut state = AppState {
+        world: World::new(),
+        simulation_running: false,
+        mouse_state: None,
+        update_ms: 16,
+        selected_body: None,
+    };
     'running: loop {
-        let mouse = event_pump.mouse_state();
         for event in event_pump.poll_iter() {
-            if !running {
-                if not_running_events(&mut world, &mut running, mouse, &event) {
+            if !state.simulation_running {
+                if not_running_events(&mut state, &event) {
                     break 'running;
                 }
-            } else if running && running_events(&mut wait_ms, &mut running, &event) {
+            } else if state.simulation_running && running_events(&mut state, &event) {
                 break 'running;
             }
         }
-        if running {
-            world.update();
+        if state.simulation_running {
+            state.world.update();
         }
         canvas.set_draw_color(Color::BLACK);
         canvas.clear();
-        world.draw(&mut canvas);
+        state.world.draw(&mut canvas, &mut font, state.selected_body);
         canvas.present();
-        std::thread::sleep(std::time::Duration::from_millis(wait_ms));
+        std::thread::sleep(std::time::Duration::from_millis(state.update_ms as u64));
     }
+    Ok(())
 }
